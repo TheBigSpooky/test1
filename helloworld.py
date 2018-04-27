7b4750d2fdb32c9c27cfbb8a2e64a00b1ff7fc8b
TheBigSpooky <thebigspooky@gmail.com>
2018-04-27 11:12:26 -0700

    hello

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,21 @@
 
 import turtle
-
-
-
+import random
 wn = turtle.Screen()
-wn.bgcolor("lightgreen")        # set the window background color
 
+#  set window colors
+wn.bgcolor("lightgreen")
+
+#  get a turtle
 tess = turtle.Turtle()
 tess.color("blue")              # make tess blue
 tess.pensize(3)                 # set the width of her pen
+tess.speed(0)
 
-tess.forward(50)
-tess.left(120)
-tess.forward(50)
+#  do stuff
+for i in range(0,1000):
+    tess.forward(4)
+    tess.left(random.randint(1,10))
 
 wn.exitonclick()                # wait for a user click on the canvas
 
